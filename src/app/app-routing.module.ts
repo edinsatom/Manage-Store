6d86dc29eff7a8c26f3f2e688957741f4c4c7380
Edinson Arteaga Alzate <edinsatom@gmail.com>
2022-09-04 11:52:42 -0500

    Create initial structure and coponents

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { CreateProductComponent } from './components/products/create-product/create-product.component';
+import { ProductsComponent } from './components/products/products/products.component';
+import { ProfileComponent } from './components/profile/profile/profile.component';
+
+import { GuardGuard } from './service/guard.guard';
+
+
+const routes: Routes = [
+  { path: 'products', component: ProductsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [ GuardGuard ] },
+  { path: 'product/:id', component: CreateProductComponent, canActivate: [ GuardGuard ] },
+  { path: '**', pathMatch: 'full', redirectTo: 'products' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products/products.component';
+import { CreateProductComponent } from './components/products/create-product/create-product.component';
+import { ProfileComponent } from './components/profile/profile/profile.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+import { ReusablesModule } from './reusables/reusables.module';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CreateProductComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReusablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
