2c186514f16ae17e5b4898ee87c4182586e32f3c
Edinson Arteaga Alzate <edinsatom@gmail.com>
2022-09-15 23:48:43 -0500

    Removing enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,20 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-
-
-
-  firebaseConfig: {
-    apiKey: "AIzaSyD0Tmek9B3wHDxIXkx9ETgSBMHiA5gTd3g",
-    authDomain: "store-66667.firebaseapp.com",
-    databaseURL: "https://store-66667-default-rtdb.firebaseio.com",
-    projectId: "store-66667",
-    storageBucket: "store-66667.appspot.com",
-    messagingSenderId: "400650789234",
-    appId: "1:400650789234:web:40f36ebe4eeb3598ab7deb"
-  }
-
+  production: false
 };
 
 /*
