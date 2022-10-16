7bd72735a73f9e7bbb3246e7b606c4190ca9a7e4
Edinson Arteaga Alzate <edinsatom@gmail.com>
2022-10-16 14:21:08 -0500

    Styles for all components

--- src/app/catalog-module/catalog.module.ts
+++ src/app/catalog-module/catalog.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CatalogComponent } from './components/catalog/catalog.component';
 import { CommonsModule } from '../common-module/commons.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
     CatalogComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
